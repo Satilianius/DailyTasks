fa56dd576a43f4d1dbc0a075f7179f93d5e00543
fedorenkodk <66945219+fedorenkodk@users.noreply.github.com>
2024-12-27 02:32:21 +0000

    Implement ProgressRepository
    - Try to use separate functions for different types of the task - caller should know the task type

--- Progress/MemoryRepository.go
+++ Progress/MemoryRepository.go
@@ -1,57 +1,78 @@
 package Progress
 
 import (
+	"DailyTasks/Tasks"
+	"errors"
 	"github.com/google/uuid"
 	"sync"
+	"time"
 )
 
 type MemoryRepository struct {
-	taskProgress map[uuid.UUID]PrintableProgress
-	mu           sync.Mutex // For thread safety
+	booleanTaskProgress map[uuid.UUID]BooleanProgress
+	numberTaskProgress  map[uuid.UUID]NumberProgress
+	// TODO mutex per map?
+	mu sync.Mutex // For thread safety
 }
 
 func NewMemoryRepository() *MemoryRepository {
 	return &MemoryRepository{
-		taskProgress: make(map[uuid.UUID]PrintableProgress),
+		booleanTaskProgress: make(map[uuid.UUID]BooleanProgress),
+		numberTaskProgress:  make(map[uuid.UUID]NumberProgress),
 	}
 }
 
-func (r *MemoryRepository) Add(progress PrintableProgress) error {
+func (r *MemoryRepository) AddBooleanTask(task Tasks.Task) error {
 	r.mu.Lock()
 	defer r.mu.Unlock()
 
-	//TODO implement me
-	panic("implement me")
+	r.booleanTaskProgress[task.Uuid] = BooleanProgress{}
+	return nil
 }
 
-func (r *MemoryRepository) GetByUuid(taskUuid uuid.UUID) (*PrintableProgress, error) {
+func (r *MemoryRepository) AddNumberTask(task Tasks.Task) error {
 	r.mu.Lock()
 	defer r.mu.Unlock()
 
-	//TODO implement me
-	panic("implement me")
+	r.numberTaskProgress[task.Uuid] = NumberProgress{}
+	return nil
 }
 
-func (r *MemoryRepository) Update(updatedProgress PrintableProgress) error {
+func (r *MemoryRepository) GetByUuid(taskUuid uuid.UUID) (PrintableProgress, error) {
 	r.mu.Lock()
 	defer r.mu.Unlock()
-
-	//TODO implement me
-	panic("implement me")
+	// TODO handle missing key
+	// TODO should caller know the task type? Here and in delete
+	if progress, exists := r.booleanTaskProgress[taskUuid]; exists {
+		return progress, nil
+	}
+	if progress, exists := r.numberTaskProgress[taskUuid]; exists {
+		return progress, nil
+	}
+	return nil, errors.New("progress not found")
 }
 
-func (r *MemoryRepository) GetAll() ([]PrintableProgress, error) {
+func (r *MemoryRepository) UpdateBooleanProgress(taskUuid uuid.UUID, date time.Time, done bool) error {
 	r.mu.Lock()
 	defer r.mu.Unlock()
 
-	//TODO implement me
-	panic("implement me")
+	r.booleanTaskProgress[taskUuid].DatesToValue[date] = done
+	return nil
+}
+
+func (r *MemoryRepository) UpdateNumberProgress(taskUuid uuid.UUID, date time.Time, value float64) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	r.numberTaskProgress[taskUuid].DatesToValue[date] = value
+	return nil
 }
 
 func (r *MemoryRepository) Remove(taskUuid uuid.UUID) error {
 	r.mu.Lock()
 	defer r.mu.Unlock()
 
-	//TODO implement me
-	panic("implement me")
+	delete(r.booleanTaskProgress, taskUuid)
+	delete(r.numberTaskProgress, taskUuid)
+	return nil
 }
--- Progress/ProgressRepository.go
+++ Progress/ProgressRepository.go
@@ -1,11 +1,17 @@
 package Progress
 
-import "github.com/google/uuid"
+import (
+	"DailyTasks/Tasks"
+	"github.com/google/uuid"
+	"time"
+)
 
 type Repository interface {
-	Add(progress PrintableProgress) error
-	GetByUuid(taskUuid uuid.UUID) (*PrintableProgress, error)
-	Update(updatedProgress PrintableProgress) error
-	GetAll() ([]PrintableProgress, error)
+	AddBooleanTask(task Tasks.Task) error
+	AddNumberTask(task Tasks.Task) error
+	GetByUuid(taskUuid uuid.UUID) (PrintableProgress, error)
+	// TODO check task type?
+	UpdateBooleanProgress(taskUuid uuid.UUID, date time.Time, done bool) error
+	UpdateNumberProgress(taskUuid uuid.UUID, date time.Time, value float64) error
 	Remove(taskUuid uuid.UUID) error
 }
