624cfc3139cddef1321571ccdfe38e59125e51c6
fedorenkodk <fedorenkodk@gmail.com>
2025-01-29 23:40:26 +0000

    Update memory repository to use new interface and Progress structs
    - Store progress directly as maps in memory and wrap the data in Progress instances only when giving it to the caller - to separate in memory data storage from the views returned
    - Implement the function to get all teh progress between time instances - this wil need to be optimized using better data structures

--- Progress/MemoryRepository.go
+++ Progress/MemoryRepository.go
@@ -2,16 +2,16 @@ package Progress
 
 import (
 	"DailyTasks/Tasks"
-	"errors"
+	"fmt"
 	"github.com/google/uuid"
 	"sync"
 	"time"
 )
 
 type MemoryRepository struct {
-	booleanTaskProgress  map[uuid.UUID]*BooleanProgress
-	numberTaskProgress   map[uuid.UUID]*NumberProgress
-	durationTaskProgress map[uuid.UUID]*DurationProgress
+	booleanTaskProgress  map[uuid.UUID]map[time.Time]bool // Missing date considered false
+	numberTaskProgress   map[uuid.UUID]map[time.Time]float64
+	durationTaskProgress map[uuid.UUID]map[time.Time]time.Duration
 
 	booleanMutex  sync.RWMutex
 	numberMutex   sync.RWMutex
@@ -20,8 +20,9 @@ type MemoryRepository struct {
 
 func NewMemoryRepository() *MemoryRepository {
 	return &MemoryRepository{
-		booleanTaskProgress: make(map[uuid.UUID]*BooleanProgress),
-		numberTaskProgress:  make(map[uuid.UUID]*NumberProgress),
+		booleanTaskProgress:  make(map[uuid.UUID]map[time.Time]bool),
+		numberTaskProgress:   make(map[uuid.UUID]map[time.Time]float64),
+		durationTaskProgress: make(map[uuid.UUID]map[time.Time]time.Duration),
 	}
 }
 
@@ -31,59 +32,137 @@ func (r *MemoryRepository) AddTask(task Tasks.Task) error {
 
 	switch task.TaskType {
 	case Tasks.BooleanTask:
-		r.booleanTaskProgress[task.Uuid] = NewBooleanProgress()
+		r.booleanTaskProgress[task.Uuid] = make(map[time.Time]bool)
 	case Tasks.NumberTask:
-		r.numberTaskProgress[task.Uuid] = NewNumberProgress()
+		r.numberTaskProgress[task.Uuid] = make(map[time.Time]float64)
 	case Tasks.DurationTask:
-		r.durationTaskProgress[task.Uuid] = NewDurationProgress()
+		r.durationTaskProgress[task.Uuid] = make(map[time.Time]time.Duration)
 	}
 
 	return nil
 }
 
-func (r *MemoryRepository) GetPrintableProgressByUuid(taskUuid uuid.UUID) (PrintableProgress, error) {
-	booleanProgress, found, err := r.GetBooleanByUuid(taskUuid)
+func (r *MemoryRepository) GetAllProgress(taskUuid uuid.UUID) (PrintableProgress, bool, error) {
+	booleanProgress, found, err := r.GetBooleanProgressAll(taskUuid)
 	if found {
-		return booleanProgress, err
+		return booleanProgress, true, err
 	}
 
-	numberProgress, found, err := r.GetNumberByUuid(taskUuid)
+	numberProgress, found, err := r.GetNumberProgressAll(taskUuid)
 	if found {
-		return numberProgress, err
+		return numberProgress, true, err
 	}
 
-	return nil, errors.New("progress not found")
+	durationProgress, found, err := r.GetDurationProgressAll(taskUuid)
+	if found {
+		return durationProgress, true, err
+	}
+
+	return nil, false, fmt.Errorf("progress for task %s not found", taskUuid)
 }
 
-func (r *MemoryRepository) GetBooleanByUuid(taskUuid uuid.UUID) (*BooleanProgress, bool, error) {
+func (r *MemoryRepository) GetProgressBetweenDates(taskUuid uuid.UUID, from time.Time, to time.Time) (PrintableProgress, bool, error) {
+	booleanProgress, found, err := r.GetBooleanProgressBetweenDates(taskUuid, from, to)
+	if found {
+		return booleanProgress, true, err
+	}
+
+	numberProgress, found, err := r.GetNumberProgressBetweenDates(taskUuid, from, to)
+	if found {
+		return numberProgress, true, err
+	}
+
+	durationProgress, found, err := r.GetDurationProgressBetweenDates(taskUuid, from, to)
+	if found {
+		return durationProgress, true, err
+	}
+
+	return nil, false, fmt.Errorf("progress for task %s not found", taskUuid)
+}
+
+func (r *MemoryRepository) GetBooleanProgressAll(taskUuid uuid.UUID) (*BooleanProgress, bool, error) {
 	r.booleanMutex.RLock()
 	defer r.booleanMutex.RUnlock()
 
 	if progress, exists := r.booleanTaskProgress[taskUuid]; exists {
-		return progress, true, nil
+		return NewBooleanProgress(progress), true, nil
 	}
 
 	return nil, false, nil
 }
 
-func (r *MemoryRepository) GetNumberByUuid(taskUuid uuid.UUID) (*NumberProgress, bool, error) {
+func (r *MemoryRepository) GetNumberProgressAll(taskUuid uuid.UUID) (*NumberProgress, bool, error) {
 	r.numberMutex.RLock()
 	defer r.numberMutex.RUnlock()
 
 	if progress, exists := r.numberTaskProgress[taskUuid]; exists {
-		return progress, true, nil
+		return NewNumberProgress(progress), true, nil
 	}
 
 	return nil, false, nil
 }
 
-func (r *MemoryRepository) GetDurationByUuid(taskUuid uuid.UUID) (*DurationProgress, bool, error) {
+func (r *MemoryRepository) GetDurationProgressAll(taskUuid uuid.UUID) (*DurationProgress, bool, error) {
 	r.durationMutex.RLock()
 	defer r.durationMutex.RUnlock()
 
 	if progress, exists := r.durationTaskProgress[taskUuid]; exists {
-		return progress, true, nil
+		return NewDurationProgress(progress), true, nil
 	}
+	return nil, false, nil
+}
+
+func (r *MemoryRepository) GetBooleanProgressBetweenDates(taskUuid uuid.UUID, from time.Time, to time.Time) (*BooleanProgress, bool, error) {
+	r.booleanMutex.RLock()
+	defer r.booleanMutex.RUnlock()
+
+	if progress, exists := r.booleanTaskProgress[taskUuid]; exists {
+		progressBetweenDates := make(map[time.Time]bool)
+		// TODO replace with more efficient structure. b-tree? sorted slice? skip list?
+		for key, value := range progress {
+			if key.Compare(from) >= 0 && key.Before(to) {
+				progressBetweenDates[key] = value
+			}
+		}
+		return NewBooleanProgress(progressBetweenDates), true, nil
+	}
+
+	return nil, false, nil
+}
+
+func (r *MemoryRepository) GetNumberProgressBetweenDates(taskUuid uuid.UUID, from time.Time, to time.Time) (*NumberProgress, bool, error) {
+	r.numberMutex.RLock()
+	defer r.numberMutex.RUnlock()
+
+	if progress, exists := r.numberTaskProgress[taskUuid]; exists {
+		progressBetweenDates := make(map[time.Time]float64)
+		// TODO replace with more efficient structure. b-tree? sorted slice? skip list?
+		for key, value := range progress {
+			if key.Compare(from) >= 0 && key.Before(to) {
+				progressBetweenDates[key] = value
+			}
+		}
+		return NewNumberProgress(progressBetweenDates), true, nil
+	}
+
+	return nil, false, nil
+}
+
+func (r *MemoryRepository) GetDurationProgressBetweenDates(taskUuid uuid.UUID, from time.Time, to time.Time) (*DurationProgress, bool, error) {
+	r.numberMutex.RLock()
+	defer r.numberMutex.RUnlock()
+
+	if progress, exists := r.durationTaskProgress[taskUuid]; exists {
+		progressBetweenDates := make(map[time.Time]time.Duration)
+		// TODO replace with more efficient structure. b-tree? sorted slice? skip list?
+		for key, value := range progress {
+			if key.Compare(from) >= 0 && key.Before(to) {
+				progressBetweenDates[key] = value
+			}
+		}
+		return NewDurationProgress(progressBetweenDates), true, nil
+	}
+
 	return nil, false, nil
 }
 
@@ -92,10 +171,11 @@ func (r *MemoryRepository) UpdateBooleanProgress(taskUuid uuid.UUID, date time.T
 	defer r.booleanMutex.Unlock()
 
 	if progress, exists := r.booleanTaskProgress[taskUuid]; exists {
-		progress.Update(date, done)
+		progress[date] = done
 		return nil
 	}
-	return errors.New("progress not found")
+
+	return fmt.Errorf("progress for boolean task %s not found", taskUuid)
 }
 
 func (r *MemoryRepository) UpdateNumberProgress(taskUuid uuid.UUID, date time.Time, value float64) error {
@@ -103,10 +183,11 @@ func (r *MemoryRepository) UpdateNumberProgress(taskUuid uuid.UUID, date time.Ti
 	defer r.numberMutex.Unlock()
 
 	if progress, exists := r.numberTaskProgress[taskUuid]; exists {
-		progress.Update(date, value)
+		progress[date] = value
 		return nil
 	}
-	return errors.New("progress not found")
+
+	return fmt.Errorf("progress for number task %s not found", taskUuid)
 }
 
 func (r *MemoryRepository) UpdateDurationProgress(taskUuid uuid.UUID, date time.Time, value time.Duration) error {
@@ -114,10 +195,11 @@ func (r *MemoryRepository) UpdateDurationProgress(taskUuid uuid.UUID, date time.
 	defer r.durationMutex.Unlock()
 
 	if progress, exists := r.durationTaskProgress[taskUuid]; exists {
-		progress.Update(date, value)
+		progress[date] = value
+		return nil
 	}
 
-	return errors.New("progress not found")
+	return fmt.Errorf("progress for duration task %s not found", taskUuid)
 }
 
 func (r *MemoryRepository) Remove(taskUuid uuid.UUID) error {
